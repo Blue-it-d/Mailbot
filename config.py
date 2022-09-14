74076a96c92df9c8a5528c5b775091cc53692482
Fadi <blueit@outloo.de>
2022-09-14 21:55:18 +0200

    Some improvements

--- config.py
+++ config.py
@@ -2,13 +2,15 @@ import getpass
 import os
 #bot_address = input ("Address to log in to: ")
 #imapserver = input ("IMAP server domain:")
-imapserver = os.getenv('SERVER_NAME')
-bot_address = os.getenv('BOT_ADDRESS')
-trusted_addresses = os.getenv("TRUSTED_SENDERS")
-print(imapserver, bot_address, trusted_addresses)
+#imapserver = os.getenv('SERVER_NAME')
+#bot_address = os.getenv('BOT_ADDRESS')
+#trusted_addresses = os.getenv("TRUSTED_SENDERS")
+imapserver = "imap-mail.outlook.com"
+bot_address = "blueit@outlook.de"
+print(imapserver, bot_address)
 ##smtpserver = input ("SMTP server domain:")
 ##smtpserverport = input ("SMTP server port [587]:")
-#if not smtpserverport or smtpserverport == "":
+# if not smtpserverport or smtpserverport == "":
 #    smtpserverport = 587
 
 pwd = getpass.getpass("Account password:")
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from config import *
 import imapclient
 import pyzmail
 
+TRUSTED_SENDER = ("test@hotmail.com", "test2@hotmail.com")
+
+
 def imap_init():
     """
     Initialize IMAP connction
@@ -11,49 +14,61 @@ def imap_init():
     global i
     i = imapclient.IMAPClient(imapserver)
     c = i.login(bot_address, pwd)
-    i.select_folder("INBOX")
+    i.select_folder("INBOX", readonly=True)
+    # i.select_folder("INBOX")
     print("DONE. ")
 
 
-## Get unreaded mails
 def get_unread():
     """
-    Fetch unread emails
+    Get unreaded mails
+    returns the mails as raw data or None if nothings found
     """
     uids = i.search(['UNSEEN'])
     if not uids:
         return None
     else:
         print("Found %s unreads" % len(uids))
+        # TODO: Remove readonly=true as these is now true
+        # for testing purpose.
         return i.fetch(uids, ['BODY[]', 'FLAGS'])
+
+
+def isValidSender(sender):
+    """
+    Check if the given sender is valid
+    It compars it with vordefined arrays  
+    """
+    if sender not in TRUSTED_SENDER:
+        print("Unread is from %s skipping" % (sender))
+        return False
+    return True
+
 
 def analyze_msg(raws, a):
     print("Analyzing message with uid " + str(a))
     msg = pyzmail.PyzMessage.factory(raws[a][b'BODY[]'])
     sender = msg.get_address("from")
-    if sender[1] != trusted_addresses:
-        print("Unread is from %s <%s> skipping" % (sender[1],sender[1]))
+    # Check if a valid sender
+    if isValidSender(sender[1]):
+        print("valid" + sender[1])
         return None
-    global subject 
-
+    global subject
     if msg.text_part is None:
         print("No text part, cannot parse")
-        return None                                              
+        return None
     text = msg.text_part.get_payload().decode(msg.text_part.charset)
-    cmds = text.replace("\r","").split("\n")
-    print(cmds)
+    cmds = text.replace("\r", "").split("\n")
+
 
 imap_init()
 msgs = get_unread()
 
 if msgs is not None:
-    print("here")
     for msg in msgs.keys():
-        print(msg)
         print(type(msg))
         if type(msg) is not int:
             continue
         cmds = analyze_msg(msgs, msg)
 else:
     print("No new Mails")
-
