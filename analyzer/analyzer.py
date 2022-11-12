0dd4f39ab0cac336953b6d7b8cc7a03170fb7f3a
blue <blueit@outlook.com>
2022-11-12 20:42:07 +0100

    Move patterns to a separated file and some fixes in function

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -3,10 +3,10 @@
 import os
 
 import spacy
-from patterns import GREETING_INPUTS
+from patterns import *
 
 TEXT = """
-Moin bot, 
+Moin Bot,
 ich muss einen Nutzer mit folgenden Informationen hinzufügen:
 Vorname: Mustermann, Nachname: Müller, geb. am: 04.04.1991, Anschrift:
 BeispielStraße 11 49074 Osnabrück, Arbeitsstunden: 30, Startdatum: 1-1-
@@ -40,17 +40,26 @@ def text_preprocessing(text):
     # this is used to save text to a file in case something goes wrong
     save_to_file(text)
 
+    
     # split text in lines and remove empty
     lines = [line for line in text.splitlines() if line.strip()]
-
-    # check if first line is empty or greeting
-    if (word in lines[0].lower for word in GREETING_INPUTS):
-        lines.pop(0)
+    # Return none if the text is empty
+    if not lines:
+        return None
+        
+    # check if first line contains a greeting word
+    for word in lines[0].split():
+        if word.lower() in GREETING_INPUTS:
+            # remove greeting from text
+            lines = lines[1:]
+            break
+    
+    if not lines:
+        return None
 
     nlp = spacy.load("de_core_news_sm")
     # split text in tokens for the first line afret the greeting
     doc = nlp(lines[0])
-
     key_list = []
     # Extracting the essential words (verbs and nouns)
     for token in doc:
@@ -59,10 +68,26 @@ def text_preprocessing(text):
             key_list.append(token.lemma_)
 
     delete_file()
-    return key_list
+    return " ".join(key_list)
 
 
-print(text_preprocessing(text))
+
+# Chatbot intent classification
+# Rule-based pattern matching
+# https://spacy.io/usage/rule-based-matching
+def intent_detection(text):
+    """Detect the intent of the given text.
+    predefined training data is used to detect the intent.
+    """
+    nlp = spacy.load("de_core_news_sm")
+    ruler = nlp.add_pipe("entity_ruler", before="ner")
+    patterns = [ADD_PATTERN, USER_PATTERN, CONTRACT_PATTERN, DEPARTMENT_PATTERN]
+    ruler.add_patterns(patterns)
+    doc = nlp(text)
+    print("ents", len(list(doc.ents)))
+        
+    print([(ent.text, ent.label_) for ent in doc.ents])
+    return [(ent.text, ent.label_) for ent in doc.ents]
 
 
 def check(text):
@@ -89,10 +114,10 @@ def check(text):
     print([(ent.text, ent.label_) for ent in doc.ents])
 
 
-""" x = tex_preproccessing(text)
-# rejoin the return list to a string
-print(x)
-x = " ".join(x)
-print(x)
-check(x)
- """
+
+processed_text = text_preprocessing(TEXT)
+ 
+if processed_text:
+     intent_detection(processed_text)
+
+# check type of a variable
